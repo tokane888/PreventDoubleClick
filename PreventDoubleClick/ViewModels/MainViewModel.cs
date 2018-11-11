5a4a6116cc9b76f728f49c85e68965a4276ee845
tokane888 <tokane888@gmail.com>
2018-11-11 15:38:24 +0900

    Revert "Added CommandExecutionPreventer to prevent command executed continuously."
    This reverts commit 4be3aa344405cdc0af4142f8202b96f8f95f72e5.

--- PreventDoubleClick/ViewModels/MainViewModel.cs
+++ PreventDoubleClick/ViewModels/MainViewModel.cs
@@ -2,7 +2,6 @@
 using Prism.Commands;
 using Prism.Windows.Mvvm;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
@@ -10,61 +9,31 @@ namespace PreventDoubleClick.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        public List<string> Numbers = new List<string> { "Zero", "One", "Two" };
-        public string SelectedItem { get; private set; }
-
         public MainViewModel()
         {
             DisplayHelloDialogCommand = new DelegateCommand(
                 async () => await DisplayHelloDialogAsync(),
                 () => _canDisplayHelloDialog);
-            DisplaySelectedItemCommand = new DelegateCommand<string>(
-                async s => await DisplaySelectedItemAsync(s),
-                _ => _canDisplaySelectedItem);
         }
 
         private bool _canDisplayHelloDialog = true;
         public DelegateCommand DisplayHelloDialogCommand { get; private set; }
         private async Task DisplayHelloDialogAsync()
         {
-            using (new CommandExecutionPreventer(DisplayHelloDialogCommand, ref _canDisplayHelloDialog))
+            try
             {
-                Debug.WriteLine("DisplayHelloDialogCommand fired.");
+                Debug.WriteLine("Command fired.");
+                _canDisplayHelloDialog = false;
+                DisplayHelloDialogCommand.RaiseCanExecuteChanged();
                 await MainPage.Current.HelloDialog.ShowAsync();
             }
-        }
-
-        private bool _canDisplaySelectedItem = true;
-        public DelegateCommand<string> DisplaySelectedItemCommand { get; private set; }
-        private async Task DisplaySelectedItemAsync(string selectedItem)
-        {
-            using (new CommandExecutionPreventer(DisplaySelectedItemCommand, ref _canDisplaySelectedItem))
+            finally
             {
-                Debug.WriteLine("DisplaySelectedItemCommand fired.");
-                SelectedItem = selectedItem;
-                await MainPage.Current.SelectedItemDialog.ShowAsync();
+                _canDisplayHelloDialog = true;
+                DisplayHelloDialogCommand.RaiseCanExecuteChanged();
             }
         }
     }
 
-    public class CommandExecutionPreventer : IDisposable
-    {
-        private readonly DelegateCommandBase _delegateCommand;
-        private bool _canExecute;
 
-        public CommandExecutionPreventer(DelegateCommandBase delegateCommand, ref bool canExecute)
-        {
-            _delegateCommand    = delegateCommand;
-            _canExecute         = canExecute;
-
-            _canExecute = false;
-            _delegateCommand?.RaiseCanExecuteChanged();
-        }
-
-        public void Dispose()
-        {
-            _canExecute = true;
-            _delegateCommand?.RaiseCanExecuteChanged();
-        }
-    }
 }
